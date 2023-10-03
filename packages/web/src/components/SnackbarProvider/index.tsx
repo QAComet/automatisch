4f89e1b047e4a58830e483060a46d0ea48511b5f
QAComet <lucas@qacomet.com>
2023-10-03 16:57:44 -0600

    test: write tests for user management

--- packages/web/src/components/SnackbarProvider/index.tsx
+++ packages/web/src/components/SnackbarProvider/index.tsx
@@ -2,9 +2,18 @@ import * as React from 'react';
 import {
   SnackbarProvider as BaseSnackbarProvider,
   SnackbarProviderProps,
+  closeSnackbar
 } from 'notistack';
 
 const SnackbarProvider = (props: SnackbarProviderProps): React.ReactElement => {
+  React.useEffect(() => {
+    if (process.env['REACT_APP_GLOBAL_SNACKBAR_CLOSE']) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      window.closeSnackbar = closeSnackbar;
+    }
+  }, []);
+  
   return (
     <BaseSnackbarProvider
       {...props}
